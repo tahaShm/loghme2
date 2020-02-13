d07c61c2ae78d4e4f66a09b512a7bdf096231bb2
Parsa Hoseini nejad <hoseininejad1999@gmail.com>
2020-02-13 19:52:42 +0330

    show cart added

---
+++ src/main/java/ie/commands/showCart.java
@@ -0,0 +1,37 @@
+package ie.commands;
+
+import ie.App;
+
+import java.util.Map;
+
+public class showCart implements Command {
+    public String handle(String body) {
+        App app = App.getInstance();
+        String content = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>User</title>\n" +
+                "    <style>\n" +
+                "        li, div, form {\n" +
+                "        \tpadding: 5px\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <div>restaurant name</div>\n" +
+                "    <ul>\n";
+        for (Map.Entry<String, Integer> entry : app.getCart().entrySet()) {
+            content += "<li>" + entry.getKey() + " : " + entry.getValue() + "</li>\n";
+        }
+        content += "    </ul>\n" +
+                "    <form action=\"\" method=\"POST\">\n" +
+                "        <button type=\"submit\">finalize</button>\n" +
+                "    </form>\n" +
+                "</body>\n" +
+                "</html>";
+        if (app.getCart().isEmpty())
+            return "No food in cart!";
+        return content;
+    }
+}
